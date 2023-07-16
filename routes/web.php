08bc2dd661b6dd2f2ba7686b273e1724bd6e3006
latifs24 <latif.s12401@gmail.com>
2023-07-16 20:12:36 +0700

    Add tailwind

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('page1-home.index');
-});
+Route::get('/', function () { return view('page1-home.index'); });
