e3660288a0b927b927b61ba1bfa107e47db597b1
latifs24 <latif.s12401@gmail.com>
2023-07-18 01:41:35 +0700

    Add tailwind

--- resources/views/page1-home/index.blade.php
+++ resources/views/page1-home/index.blade.php
@@ -1,111 +1,86 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neki-Store : Home</title>
     @vite('resources/css/app.css')
-    <style>
-        .navbar ul li::after {
-            content: '';
-            height: 3px;
-            width: 0;
-            background: firebrick;
-            position: absolute;
-            left: 0;
-            bottom: -7px;
-            transition: 0.35s;
-        }
-
-        .navbar ul li:hover:after {
-            width: 100%;
-        }
-    </style>
 </head>
 <body>
 <section id="header">
-    <nav class="border-gray-600 bg-gray-900">
+    <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-            <div class="flex items-center">
-                <img src="{{asset('assets/img/home/neki-logo.png')}}" class="h-9 mr-3" alt="Neki-Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Neki Store</span>
-            </div>
-            <div class="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1">
-                <ul class="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:border-0">
+            <a href="/" class="flex items-center">
+                <img src="{{asset('assets/img/home/neki-logo.png')}}" class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Neki Store</span>
+            </a>
+            <button data-collapse-toggle="nav-menu" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            <div id="nav-menu" class="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="#" class="relative inline-block group">
-                            <span class="relative block py-2 pl-3 pr-4 text-white hover:text-blue-400 md:p-0 md:dark:hover:text-blue-400" aria-current="page">Home</span>
-                            <span class="absolute w-full h-1 bg-blue-500 transform origin-left scale-x-0 transition-transform duration-200 ease-in-out group-hover:scale-x-100"></span>
-                        </a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
                     </li>
                     <li>
-                        <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown"
-                                class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white md:w-auto md:border-0
-                                md:hover:text-blue-500 md:p-0 dark:text-white ">Categories
-                            <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <button id="nav-dropdown-button" data-dropdown-toggle="dropdown-content" data-dropdown-trigger="hover" class="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Categories <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true"  fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                             </svg></button>
                     </li>
                     <li>
-                        <a href="#" class="relative inline-block group">
-                            <span class="relative block py-2 pl-3 pr-4 text-white hover:text-blue-400 md:p-0 md:dark:hover:text-blue-400" aria-current="page">Top Product</span>
-                            <span class="absolute w-full h-1 bg-blue-500 transform origin-left scale-x-0 transition-transform duration-200 ease-in-out group-hover:scale-x-100"></span>
-                        </a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Top Product</a>
                     </li>
                     <li>
-                        <a href="#" class="relative inline-block group">
-                            <span class="relative block py-2 pl-3 pr-4 text-white hover:text-blue-400 md:p-0 md:dark:hover:text-blue-400" aria-current="page">Brand</span>
-                            <span class="absolute w-full h-1 bg-blue-500 transform origin-left scale-x-0 transition-transform duration-200 ease-in-out group-hover:scale-x-100"></span>
-                        </a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Catalog</a>
                     </li>
                     <li>
-                        <a href="#" class="relative inline-block group">
-                            <span class="relative block py-2 pl-3 pr-4 text-white hover:text-blue-400 md:p-0 md:dark:hover:text-blue-400" aria-current="page">Official Store</span>
-                            <span class="absolute w-full h-1 bg-blue-500 transform origin-left scale-x-0 transition-transform duration-200 ease-in-out group-hover:scale-x-100"></span>
-                        </a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Visit Us</a>
                     </li>
                 </ul>
             </div>
         </div>
-        <div id="dropdown1-menu" class="transition mt-1 border-gray-300 shadow-sm bg-cyan-50 border-y dark:bg-gray-800 dark:border-gray-600">
+        <div id="dropdown-menu" class="hidden animate-fade-in mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white dark:md:bg-gray-600 border-y dark:bg-gray-800 dark:border-gray-600">
             <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
                 <ul>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu1</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content1</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content1.</span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu2</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content2</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content2.</span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu3</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content3</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content3.</span>
                         </a>
                     </li>
                 </ul>
                 <ul>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu4</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content4</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content4.</span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu5</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content5</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content5.</span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <div class="font-semibold">Menu6</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Foo</span>
+                            <div class="font-semibold">Content6</div>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Field Content6</span>
                         </a>
                     </li>
                 </ul>
@@ -113,26 +88,71 @@
         </div>
     </nav>
 </section>
+<section id="main" class="w-full">
+    <div class="w-1/2 border-2 border-amber-400">
+        Moh.Alu
+    </div>
 
+</section>
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const button = document.querySelector('[data-collapse-toggle="nav-menu"]');
+        const navbar = document.getElementById('nav-menu');
+        const navMenu = document.getElementById('dropdown-menu');
 
-{{--<div class="relative mx-auto w-full max-w-md sm:max-w-xl md:max-w-full pt-6">--}}
-{{--    <div class="w-60 h-60 mx-auto rounded-full overflow-hidden border-4 border-blue-600">--}}
-{{--        <div class="transform transition-transform--}}
-{{--        duration-500 ease-in-out scale-110 hover:scale-150">--}}
-{{--            <img src="{{asset('assets/img/home/noa-profile.jpg')}}" width="270" height="270">--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="mt-2 sm:mt-10 sm:m-8 border-t border-gray-400 pt-3 text-center">--}}
-{{--        <div class="border-b border-gray-400 pb-3">--}}
-{{--            <p class="text-black start-1 font-bold md:text-center">Ushio <span class="text-sky-600"> Noa</span></p>--}}
-{{--        </div>--}}
-{{--        <div class="md:text-left md:mx-10 md:mt-3">--}}
-{{--            Ushio Noa adalah siswa Millennium Sience School tahun ke-2, dia adalah seorang--}}
-{{--            Sekretaris di klub Seminar Millennium dan merupakan murid kesayangan gwe &#9829;--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+        button.addEventListener('click', function() {
+            if (!navMenu.classList.contains('hidden')){
+                navMenu.classList.add('hidden');
+            }
+            navbar.classList.toggle('hidden');
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const button = document.getElementById('nav-dropdown-button');
+        const dropdown = document.getElementById('dropdown-menu');
+        let timeoutId;
+
+        function showDropdown() {
+            clearTimeout(timeoutId);
+            if (window.innerWidth >= 768) {
+                dropdown.classList.remove('hidden');
+                dropdown.classList.remove('animate-fade-out');
+            }
+        }
+
+        function hideDropdown() {
+            timeoutId = setTimeout(function() {
+                if (window.innerWidth >= 768) {
+                    dropdown.classList.add('animate-fade-out');
+                    setTimeout(function() {
+                        dropdown.classList.add('hidden');
+                        dropdown.classList.remove('animate-fade-out');
+                    }, 100);
+                }
+            }, 100);
+        }
+
+        function toggleDropdown() {
+            if (window.innerWidth < 768) {
+                dropdown.classList.toggle('hidden');
+                dropdown.classList.remove('animate-fade-out');
+            }
+        }
+
+        button.addEventListener('click', toggleDropdown);
+        button.addEventListener('mouseover', showDropdown);
+        button.addEventListener('mouseout', hideDropdown);
+        dropdown.addEventListener('mouseover', showDropdown);
+        dropdown.addEventListener('mouseout', hideDropdown);
+
+        window.addEventListener('resize', function() {
+            hideDropdown();
+        });
+    });
+
+</script>
+
 </body>
-
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./resources/views/**/*.blade.php",
@@ -6,7 +5,32 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'fade-in': 'fade-in 0.3s ease-in',
+                'fade-in-down': 'fade-in-down 0.3s ease-out',
+                'fade-out': 'fade-out 0.3s ease-out',
+                'fade-out-up': 'fade-out-up 0.3s ease-out',
+            },
+            keyframes: {
+                'fade-in': {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' },
+                },
+                'fade-in-down': {
+                    '0%': { opacity: '0', transform: 'translateY(-10px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+                'fade-out': {
+                    '0%': { opacity: '1' },
+                    '100%': { opacity: '0' },
+                },
+                'fade-out-up': {
+                    '0%': { opacity: '1', transform: 'translateY(0)' },
+                    '100%': { opacity: '0', transform: 'translateY(-10px)' },
+                },
+            },
+        },
     },
     plugins: [],
-}
+};
